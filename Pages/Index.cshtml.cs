ce31a60a6d31cc88a01db3dfcfae9b7634e43d9b
Aayushi5899 <W0787383@myscc.ca>
2021-09-23 15:09:51 -0400

    for loop for the array

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -11,6 +11,7 @@ namespace Travelling.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        DateTime todaysDate;
 
         public IndexModel(ILogger<IndexModel> logger)
         {
@@ -19,7 +20,7 @@ namespace Travelling.Pages
 
         public void OnGet()
         {
-
+          DateTime todaysDate = DateTime.Now;
         }
     }
 }
